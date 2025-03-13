a8ccc854cec6e11e79e642345113da220eef36f4
IT21161674 <it21161674@my.sliit.lk>
2025-03-13 11:37:48 +0530

    Behavior backend url added

--- frontend/lib/constants/constants.dart
+++ frontend/lib/constants/constants.dart
@@ -1,3 +1,4 @@
 class AppConstants {
   static const String BASE_URL_EYE_ANALYSIS = "https://5cd4-34-143-131-3.ngrok-free.app/";// Chirath
+  static const String BASE_URL_BEHAVIORS = "https://f5c2-34-86-198-103.ngrok-free.app/"; //Poorna
 }
